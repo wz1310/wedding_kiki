0106fd2bd0986d8ca0f7cfb4ea487dfbd8abad43
wz1310 <a002041zano@gmail.com>
2026-02-01 01:24:53 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
+        base: '/wedding_kiki/',
         port: 3000,
         host: '0.0.0.0',
       },
